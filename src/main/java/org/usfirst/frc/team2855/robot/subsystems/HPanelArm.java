b8436f7dc1d474ce9b271ecc1032596967d7ad45
BEASTBot <beastbot2855@gmail.com>
2019-02-16 17:34:20 -0600

    Update HPanelArm

--- src/main/java/org/usfirst/frc/team2855/robot/subsystems/HPanelArm.java
+++ src/main/java/org/usfirst/frc/team2855/robot/subsystems/HPanelArm.java
@@ -26,26 +26,26 @@ public class HPanelArm extends Subsystem {
     }
 
     public void init() {
-        int pidLoopIdx = 0;
-        int timeoutMs = 30;
-        double kF = 0.0;
-        double kP = 0.0;
-        double kI = 0.0;
-        double kD = 0.0;
-        hatchSpx.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative, pidLoopIdx, timeoutMs);
-        hatchSpx.setSensorPhase(true);
-        hatchSpx.setInverted(false);
-        hatchSpx.configNominalOutputForward(0, timeoutMs);
-        hatchSpx.configNominalOutputReverse(0, timeoutMs);
-        hatchSpx.configPeakOutputForward(1, timeoutMs);
-        hatchSpx.configPeakOutputReverse(-1, timeoutMs);
-        hatchSpx.configAllowableClosedloopError(pidLoopIdx, 0, timeoutMs);
-        hatchSpx.config_kF(pidLoopIdx, kF, timeoutMs);
-        hatchSpx.config_kP(pidLoopIdx, kP, timeoutMs);
-        hatchSpx.config_kI(pidLoopIdx, kI, timeoutMs);
-        hatchSpx.config_kD(pidLoopIdx, kD, timeoutMs);
-        int absolutePosition = hatchSpx.getSensorCollection().getPulseWidthPosition() & 0xfff * -1;
-        hatchSpx.setSelectedSensorPosition(absolutePosition, pidLoopIdx, timeoutMs);
+        // int pidLoopIdx = 0;
+        // int timeoutMs = 30;
+        // double kF = 0.0;
+        // double kP = 0.0;
+        // double kI = 0.0;
+        // double kD = 0.0;
+        // hatchSpx.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative, pidLoopIdx, timeoutMs);
+        // hatchSpx.setSensorPhase(true);
+        // hatchSpx.setInverted(false);
+        // hatchSpx.configNominalOutputForward(0, timeoutMs);
+        // hatchSpx.configNominalOutputReverse(0, timeoutMs);
+        // hatchSpx.configPeakOutputForward(1, timeoutMs);
+        // hatchSpx.configPeakOutputReverse(-1, timeoutMs);
+        // hatchSpx.configAllowableClosedloopError(pidLoopIdx, 0, timeoutMs);
+        // hatchSpx.config_kF(pidLoopIdx, kF, timeoutMs);
+        // hatchSpx.config_kP(pidLoopIdx, kP, timeoutMs);
+        // hatchSpx.config_kI(pidLoopIdx, kI, timeoutMs);
+        // hatchSpx.config_kD(pidLoopIdx, kD, timeoutMs);
+        // int absolutePosition = hatchSpx.getSensorCollection().getPulseWidthPosition() & 0xfff * -1;
+        // hatchSpx.setSelectedSensorPosition(absolutePosition, pidLoopIdx, timeoutMs);
     }
 
     public void HPanelUp() {
