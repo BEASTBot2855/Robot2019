27fc93827792f87db897f4720554759de720bae6
BEASTBot <beastbot2855@gmail.com>
2019-02-16 14:36:44 -0600

    Update hatch panel arm

--- src/main/java/org/usfirst/frc/team2855/robot/subsystems/HPanelArm.java
+++ src/main/java/org/usfirst/frc/team2855/robot/subsystems/HPanelArm.java
@@ -1,12 +1,13 @@
 package org.usfirst.frc.team2855.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
 import org.usfirst.frc.team2855.robot.RobotMap;
 
-import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
  *
@@ -17,17 +18,40 @@ public class HPanelArm extends Subsystem {
     // here. Call these from Commands.
 
     private static WPI_TalonSRX hatchSpx = RobotMap.hatchSpx;
-    
+
     public void initDefaultCommand() {
         // Set the default command for a subsystem here.
-        //setDefaultCommand(new MySpecialCommand());
+        // setDefaultCommand(new MySpecialCommand());
         hatchSpx.set(ControlMode.Position, 10.0);
     }
-    
-    public void HPanelUp() {
-    	HPanelMove(1.0);
+
+    public void init() {
+        int pidLoopIdx = 0;
+        int timeoutMs = 30;
+        double kF = 0.0;
+        double kP = 0.0;
+        double kI = 0.0;
+        double kD = 0.0;
+        hatchSpx.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative, pidLoopIdx, timeoutMs);
+        hatchSpx.setSensorPhase(true);
+        hatchSpx.setInverted(false);
+        hatchSpx.configNominalOutputForward(0, timeoutMs);
+        hatchSpx.configNominalOutputReverse(0, timeoutMs);
+        hatchSpx.configPeakOutputForward(1, timeoutMs);
+        hatchSpx.configPeakOutputReverse(-1, timeoutMs);
+        hatchSpx.configAllowableClosedloopError(pidLoopIdx, 0, timeoutMs);
+        hatchSpx.config_kF(pidLoopIdx, kF, timeoutMs);
+        hatchSpx.config_kP(pidLoopIdx, kP, timeoutMs);
+        hatchSpx.config_kI(pidLoopIdx, kI, timeoutMs);
+        hatchSpx.config_kD(pidLoopIdx, kD, timeoutMs);
+        int absolutePosition = hatchSpx.getSensorCollection().getPulseWidthPosition() & 0xfff * -1;
+        hatchSpx.setSelectedSensorPosition(absolutePosition, pidLoopIdx, timeoutMs);
     }
-    
+
+    public void HPanelUp() {
+        HPanelMove(1.0);
+    }
+
     public void HPanelDown() {
         HPanelMove(-1.0);
     }
@@ -38,6 +62,6 @@ public class HPanelArm extends Subsystem {
 
     private void HPanelMove(double value) {
         hatchSpx.set(value);
+        SmartDashboard.putNumber("Hatch Encoder", hatchSpx.getSelectedSensorPosition());
     }
 }
-
